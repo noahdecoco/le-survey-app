2b74ded9f6793e03bd04032f37ec59cb2d7300e3
Noah Fernandes <>
2017-10-08 15:09:12 +0200

    more data

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     console.log(req);
-    res.send({ hello: 'world' });
+    res.send({
+        hello: 'world',
+        goodbye: 'world'
+    });
 });
 
 app.listen(PORT, () => {
