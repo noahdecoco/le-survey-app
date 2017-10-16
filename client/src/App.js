c4839b4c1edab4289979562945e18df3318699bc
Noah Fernandes <>
2017-10-16 23:04:09 +0200

    connected react server with express server using proxy

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Hellooo...</h1>
+                </header>
+                <br />
+                <a href="/auth/google">Sign in with Google</a>
+            </div>
+        );
+    }
+}
+
+export default App;
